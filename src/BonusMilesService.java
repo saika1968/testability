446edc0fb256c17620a75da17eba0028a160c351
Andrey Murzin <saika@bk.ru>
2020-11-07 11:19:27 +0300

    zatupil

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,10 +2,6 @@ public class BonusMilesService {
     public int calculate(int price) {
         int bonus = 20;
         int miles = price / bonus;
-        if (price > bonus) {
-        } else {
-            miles = 0;
-        }
         return miles;
     }
 }
